ae1440eb7aed4ecea037c524ea307fceb4136551
girihanbudi <Ghanbudi@gmail.com>
2023-05-01 20:37:09 +0700

    added upload file helper

---
+++ db/seed/seeder/helper.go
@@ -0,0 +1,41 @@
+package seeder
+
+import (
+	"airbnb-property-be/internal/pkg/aws/bucket"
+	"io/ioutil"
+)
+
+type UploadMeta struct {
+	Code   string
+	Path   string
+	Groups []string
+}
+
+func UploadFromPath(bucket bucket.Engine, path string, groups ...string) (*string, error) {
+	f, err := ioutil.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+	return bucket.Upload(f, groups...)
+}
+
+func BulkUploadFromPath(bucket bucket.Engine, uploadMeta []UploadMeta) (map[string]string, error) {
+	keys := map[string]string{}
+	for _, meta := range uploadMeta {
+		name, err := UploadFromPath(bucket, meta.Path, meta.Groups...)
+		if err != nil {
+			return keys, err
+		}
+		keys[meta.Code] = *name
+	}
+
+	return keys, nil
+}
+
+func MakeUploadMeta(code, path string, groups ...string) UploadMeta {
+	return UploadMeta{
+		Code:   code,
+		Path:   path,
+		Groups: groups,
+	}
+}
