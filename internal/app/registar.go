8897a7ad4726a5128ffeccc20a4b332e1cecf092
girihanbudi <Ghanbudi@gmail.com>
2023-05-01 19:44:06 +0700

    added app router

---
+++ internal/app/registar.go
@@ -0,0 +1,38 @@
+package app
+
+import (
+	_ "airbnb-property-be/docs"
+	"airbnb-property-be/graph"
+	gqlproperty "airbnb-property-be/internal/app/property/api/gql"
+
+	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/gin-gonic/gin"
+
+	swaggerfiles "github.com/swaggo/files"
+	ginswagger "github.com/swaggo/gin-swagger"
+)
+
+// Defining the Graphql handler
+func graphqlHandler(propertyHandler gqlproperty.Handler) gin.HandlerFunc {
+	// NewExecutableSchema and Config are in the generated.go file
+	// Resolver is in the resolver.go file
+	h := handler.NewDefaultServer(graph.NewExecutableSchema(
+		graph.Config{Resolvers: &graph.Resolver{
+			Property: propertyHandler,
+		}}))
+	return func(c *gin.Context) {
+		h.ServeHTTP(c.Writer, c.Request)
+	}
+}
+
+func (a App) registerHttpHandler() {
+	// register auth handler
+	a.PropertyRestHandler.RegisterApi()
+
+	// register modules to graph solver handler
+	a.HttpServer.Router.GET("/graph", graphqlHandler(
+		*a.PropertyGqlHandler,
+	))
+
+	a.HttpServer.Router.GET("/docs/*any", ginswagger.WrapHandler(swaggerfiles.Handler))
+}
