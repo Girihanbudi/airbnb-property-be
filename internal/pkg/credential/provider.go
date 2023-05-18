a8e76c6c60982dd00a55a063baa71ce6339e1b2c
girihanbudi <Ghanbudi@gmail.com>
2023-05-18 22:24:51 +0700

    added credential package

---
+++ internal/pkg/credential/provider.go
@@ -0,0 +1,43 @@
+package credential
+
+import (
+	"airbnb-property-be/internal/pkg/credential/config"
+	"airbnb-property-be/internal/pkg/env"
+	"crypto/tls"
+)
+
+type Options struct {
+	config.Config
+}
+
+type TlsCredentials struct {
+	Options
+	TlsCerts  *[]tls.Certificate
+	TlsConfig *tls.Config
+}
+
+func NewTLSCredentials(options Options) (creds TlsCredentials) {
+	creds.Options = options
+
+	// Load server's certificate and private key
+	serverCert, err := tls.LoadX509KeyPair(options.PublicCert, options.PrivateKey)
+	if err != nil {
+		return creds
+	}
+
+	// Create the credentials and return it
+	config := &tls.Config{
+		Certificates: []tls.Certificate{serverCert},
+	}
+	if env.CONFIG.Stage == string(env.StageLocal) {
+		config.ClientAuth = tls.NoClientCert
+	} else {
+		config.ClientAuth = tls.RequireAndVerifyClientCert
+	}
+
+	tlsCerts := []tls.Certificate{serverCert}
+	creds.TlsCerts = &tlsCerts
+	creds.TlsConfig = config
+
+	return creds
+}
