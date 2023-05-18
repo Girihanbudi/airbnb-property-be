621a5de10ad9e42886ff4eb8bd32e583d87cf169
girihanbudi <Ghanbudi@gmail.com>
2023-05-18 22:28:55 +0700

    restructure env

--- internal/pkg/env/config/config.go
+++ internal/pkg/env/config/config.go
@@ -3,6 +3,7 @@ package config
 import (
 	aws "airbnb-property-be/internal/pkg/aws/config"
 	cache "airbnb-property-be/internal/pkg/cache/config"
+	credential "airbnb-property-be/internal/pkg/credential/config"
 	elastic "airbnb-property-be/internal/pkg/elasticsearch/config"
 	gorm "airbnb-property-be/internal/pkg/gorm/config"
 	httpserver "airbnb-property-be/internal/pkg/http/server/config"
@@ -11,14 +12,15 @@ import (
 )
 
 type Config struct {
-	Origins    []string          `mapstructure:"origins"`
 	Stage      string            `mapstructure:"stage"`
+	Origins    []string          `mapstructure:"origins"`
 	Domain     string            `mapstructure:"domain"`
+	Creds      credential.Config `mapstructure:"creds"`
 	HttpServer httpserver.Config `mapstructure:"httpserver"`
 	DB         gorm.Config       `mapstructure:"db"`
 	Jwt        jwt.Config        `mapstructure:"jwt"`
 	Cache      cache.Config      `mapstructure:"cache"`
-	Kafka      kafka.Config      `mapstructure:"kafka"`
-	Aws        aws.Config        `mapstructure:"aws"`
 	Elastic    elastic.Config    `mapstructure:"elastic"`
+	Aws        aws.Config        `mapstructure:"aws"`
+	Kafka      kafka.Config      `mapstructure:"kafka"`
 }
